d1db12276a0fcaa6a34a347e424a8a5f864ce313
wickieonya <wickieonya@gmail.com>
2020-01-20 12:08:37 +0300

    updated computations

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const numberWithCommas = x =>
   x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-let mont_slider = document.getElementById("myRange");
+let months_slider = document.getElementById("myRange");
 let months_text = document.getElementById("months");
 
 let principal_des = document.getElementById("principal-des");
@@ -16,13 +16,14 @@ let interest_14 = document.getElementById("interest_14");
 let stawi_savings_13 = document.getElementById("stawi_savings_13");
 let stawi_savings_14 = document.getElementById("stawi_savings_14");
 
-const compute_stawi_savings = rate => {
-  let stawing_interest_and_charges = "";
-};
-
-mont_slider.oninput = function() {
+months_slider.oninput = function() {
   let month = $("#myRange").val();
-  months_text.textContent = month + " months.";
+  if (parseInt(month) > 1) {
+    console.log(month);
+    months_text.textContent = month + " months.";
+  } else {
+    months_text.textContent = month + " month.";
+  }
   repayment_period.textContent = month;
 };
 
@@ -78,57 +79,57 @@ $(".digits").click(function() {
       break;
   }
 
-  let amount = parseInt(currentInput.textContent);
+  let amount = parseFloat(currentInput.textContent).toFixed(2);
 
   if (amount >= 30000 && amount <= 250000) {
     $("#loan_amount").css("color", "white");
-    // computeInterest(principal, period, interest_rate);
+    // computeInterest(principal, period, rate=9);
     let months = parseInt($("#myRange").val());
-    principal_des.innerText = "Principal: KSH " + amount;
+    principal_des.innerText = "Principal: KSH " + numberWithCommas(amount);
 
-    let result = computeInterest(amount, months, 9);
-    let fees = result.facility_fee + result.credit_life + result.excise_duty;
-    total_fees.innerText = fees;
-
-    interest.innerText = result.interest;
-
-    repayment_amount.innerText = result.principal_and_interest + fees;
-
-    console.table(result);
+    // results object, default rate is 9%
+    let result = computeInterest(amount, months);
+    total_fees.innerText = numberWithCommas(result.total_fees);
+    interest.innerText = numberWithCommas(result.interest);
+    repayment_amount.innerText = numberWithCommas(
+      result.principal_and_interest
+    );
 
     let results = {
-      result_13: computeInterest(amount, months, 13),
-      result_14: computeInterest(amount, months, 14)
+      result_13: computeInterest(amount, months, (rate = 13)),
+      result_14: computeInterest(amount, months, (rate = 14))
     };
 
-    interest_13.innerText = parseFloat(results.result_13.interest).toFixed(2);
-    interest_14.innerText = parseFloat(results.result_14.interest).toFixed(2);
-    stawi_savings_13.innerText = parseFloat(
-      flastLoanCost(amount, 14, months) - result.interest
-    ).toFixed(2);
-    stawi_savings_14.innerText = parseFloat(
-      flastLoanCost(amount, 13, months) - result.interest
-    ).toFixed(2);
+    interest_13.innerText = (results.result_13.interest).toFixed(2);
+    interest_14.innerText = (results.result_14.interest).toFixed(2);
 
-    console.table(result);
-
-    console.log(flastLoanCost(amount, 13, months));
+    stawi_savings_13.innerText =
+      (results.result_13.total_fees - result.total_fees).toFixed(2);
+    stawi_savings_14.innerText =
+      (results.result_14.total_fees - result.total_fees).toFixed(2);
   } else {
     $("#loan_amount").css("color", "red");
   }
 });
 
-const computeInterest = (principal, months, rate) => {
-  let interest = principal * (rate / 100) * (months / 12);
-  return {
-    interest: interest,
-    principal_and_interest: parseFloat(interest + principal),
-    facility_fee: 0.02 * principal,
-    credit_life: 0.007 * principal,
-    excise_duty: +(0.1 * (0.02 * 0.007 * principal)).toFixed(2)
+const computeInterest = (
+  loan_value,
+  period,
+  rate = 9,
+  facility_fee_rate = 0.02,
+  credit_life_rate = 0.007
+) => {
+  let interest = Number(loan_value * (rate / 100) * (period / 12));
+  let result = {
+    interest,
+    principal_and_interest: interest + Number(loan_value),
+    facility_fee: facility_fee_rate * Number(loan_value),
+    credit_life: credit_life_rate * Number(loan_value)
   };
-};
-
-const flastLoanCost = (principal, rate, months) => {
-  return parseFloat((principal *= (rate / 100) * (months / 12)));
+  result.total_fees = Number(
+    result.credit_life + result.facility_fee + result.interest
+  );
+  console.log("result object table");
+  console.table(result);
+  return result;
 };
